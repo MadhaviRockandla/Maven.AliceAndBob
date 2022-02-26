a82f2542dada9d0291663ae279351c45da5a61df
madhavi <rokandla@gmail.com>
2022-02-25 23:19:46 -0500

    AliceandBob Lab Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        String Name;
+        System.out.print("Please insert your name:  ");
+        String result = scanner.nextLine();
+        System.out.println("Your name is " + result);
 
     }
 }
